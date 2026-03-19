17b2facb522f3b559ce5fed3e4b1c779835c925d
Pedro <pedmarvel@gmail.com>
2026-03-19 19:55:11 +0100

    reg_sesion casi terminado

---
+++ Reg_sesion.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<string.h>
+#include"Reg_sesion.h"
+
+int inicio_sesion(Registro reg[]);
+void realizar_reg(Registro reg[]);
+
+int inicio_sesion(Registro reg[]){               //los parametros los tengo que estudiar todavía
+
+
+     char id_intro[3], nombre_intro[21], clave_intro[9];
+     int condicion_loop=1, condicion_id=0, condicion_nombre=0, condicion_clave=0, condicion_if_loop=0, repetir=0;
+
+     while(condicion_loop==1){
+          //se introduce el id de usuario
+          printf("Introduce el id de jugador:\n");
+          fgets(id_intro, 3, stdin);
+
+          //se introduce el nombre de usuario
+          printf("Introduce el nombre de usuario:\n");
+          fgets(nombre_intro, 21, stdin);
+
+          //se introduce la contrasena del usuario
+          printf("Introduce la clave:\n");
+          fgets(clave_intro, 9, stdin);
+
+          int lengh = sizeof(reg);
+
+          //comprobamos que coinciden los parametros del usuario
+          for(int n=1; n<=lengh; n++){
+               condicion_id=0;
+               condicion_nombre=0;
+               condicion_clave=0;
+               if(strcmp(id_intro, reg[n].Id)==0)
+                    condicion_id=1;
+               if(strcmp(nombre_intro, reg[n].Nombre)==0)
+                    condicion_nombre=1;
+               if(strcmp(clave_intro, reg[n].Clave)==0)
+                    condicion_clave=1;
+               if(condicion_id==1 || condicion_nombre==1 || condicion_clave==1){
+                    condicion_loop=0;
+                    break;
+               }
+          }
+          if(condicion_loop==0){
+               break;
+          }
+          printf("Los datos introducidos no son correctos, ¿quieres introducir los datos de nuevo o salir del inicio de sesion?: marque 1 para introducir datos o 2 para salir\n ");
+          scanf("%i", &repetir);
+          if(repetir==2)
+          break;
+     }
+     //se ha iniciado correctamente sesion
+     if(condicion_loop==0)
+          return 1;
+     //no se ha iniciado sesion pero el usuario ha elegido salir
+     return 0;
+}
+
+void realizar_reg(Registro reg[]){
+
+     char id_intro[3], nombre_intro[21], clave_intro[9];
+     int condicion_name=1;
+     int lenght=sizeof(reg);
+
+     printf("Introduce un nombre:\n");
+     fgets(id_intro, 21, stdin);
+     for(int n=0; n<=lenght; n++){
+          if
+
+     }
+
+
+
+
+
+}
--- Reg_sesion.h
+++ Reg_sesion.h
@@ -1,9 +1,13 @@
-#ifndef REG_SESION.H
-#define REG_SESION.H
+#ifndef REG_SESION_H
+#define REG_SESION_H
 
 typedef struct{
     int Id[3];
     char Nombre[21];
-    char Contrasena[9];
+    char Clave[9];
 } Registro;
+
+int inicio_sesion(Registro reg[]); //devuelve un valor dependiendo de si ha iniciado sesion correctamente o no
+void realizar_reg(Registro reg[]); //registra a un nuevo usuario almacenando
+
 #endif
