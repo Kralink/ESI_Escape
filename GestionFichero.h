aba5d89342d0896aee77ac515cd9c94ed5e9e577
Manuel-Fedriani <manuel.fedrianicodeso@alum.uca.es>
2026-04-09 19:55:04 +0200

    Nuevas funciones y añadir parametros

--- GestionFichero.h
+++ GestionFichero.h
@@ -15,17 +15,17 @@
      FUNCIONES DE CARGA
 =============================
 */
-void cargar_salas();
+void cargar_salas(const char *archivo, Sala **salas, int *num_salas);
 
-void cargar_conexiones();
+void cargar_conexiones(const char *archivo, Conexion **conexiones, int *num_conexiones);
 
-void cargar_objetos();
+void cargar_objetos(const char *archivo, Objeto **objetos, int *num_objetos);
 
-void cargar_puzles();
+void cargar_puzles(const char *archivo, Puzzle **puzles, int *num_puzles);
 
-void cargar_registro();
+void cargar_registro(const char *archivo, Registro **r);
 
-
+void cargar_jugador(const char *archivo, Jugador **jugador);
 
 /*
 =============================
@@ -33,14 +33,16 @@ void cargar_registro();
 =============================
 */
 
-void guardar_salas();
+void guardar_salas(const char *archivo, const Sala *salas, int num_salas);
 
-void guardar_conexiones();
+void guardar_conexiones(const char *archivo, const Conexion *conexiones, int num_conexiones);
 
-void guardar_objetos();
+void guardar_objetos(const char *archivo, const Objeto *objetos, int num_objetos);
 
-void guardar_puzles(); 
+void guardar_puzles(const char *archivo, const Puzzle *puzles, int num_puzles);
 
-void guardar_registro();
+void guardar_registro(const char *archivo, const Registro *r);
+
+void guardar_jugador(const char *archivo, const Jugador *jugador);
 
 #endif //GESTIONFICHERO_H
